2da6a1ff4b1cad63c4820bf6a11b4b7c7f1efb2c
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-03-02 17:22:14 +0100

    initialize repository

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    require "functions.php";
+    require "router.php";
+?>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
---
+++ router.php
@@ -0,0 +1,19 @@
+<?php
+
+$routes = [
+    "/" => "controllers/home.php",
+];
+
+$uri = parse_url($_SERVER["REQUEST_URI"])["path"];
+
+routeToController($uri, $routes);
+
+function routeToController($uri, $routes) {
+    if (array_key_exists($uri, $routes)){
+        require $routes[$uri];
+    } else {
+        http_response_code(404);
+        echo "Page not found";
+        die();
+    }
+}
