76137df83278d547d6711445e90dfc9c4bd0cc2e
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-03-05 18:07:54 +0100

    Create `hero-section` for desktop

---
+++ views/partials/hero.partial/hero.php
@@ -0,0 +1,20 @@
+<head>
+  <link rel="stylesheet" type="text/css" href="./views/partials/hero.partial/hero.css">
+</head>
+
+<section class="hero-section">
+  <!-- 
+    <button class="get-started-btn" type="button">get started</button>
+  -->
+  <img class="hero-section-bg" src="./assets/images/hero-bg.png" aria-label="Hero section image">
+  <div class="hero-content">
+    <h1 class="tagline">Use <span>RollCall</span> to take the attendance call</h1>
+    <p class="smaller-tagline">Your presence is valuable - keep it organized with ease</p>
+
+    <form class="white-rectangle">
+        <input type="email" name="email" id="email" placeholder="your email address" required>
+        <button id="get-started" type="button" aria-label="Get Started">Get Started</button>
+    </form>
+
+  </div>
+</section>
