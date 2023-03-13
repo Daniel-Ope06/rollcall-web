0ba7d38630d42112136cefcb527f8a365511e758
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-03-13 14:19:58 +0100

    Add `get_user()` and `get_course()`

--- rollcallDB.php
+++ rollcallDB.php
@@ -7,10 +7,25 @@ if (!$connection){
     echo "Connection error: " . mysqli_connect_error();
 }
 
-$sql = 'SELECT email, password FROM users';
-$result = mysqli_query($connection, $sql);
-$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//$sql_users = 'SELECT email, password FROM users';
+//$result = mysqli_query($connection, $sql);
+//$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
 //mysqli_free_result($result);
 //mysqli_close($connection);
 //print_r($users);
+
+
+
+function get_user($email){
+    global $connection;
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    return (mysqli_fetch_all(mysqli_query($connection, $sql), MYSQLI_ASSOC)[0]);
+}
+
+function get_course($id){
+    global $connection;
+    $sql = "SELECT * FROM courses WHERE id = '$id'";
+    return (mysqli_fetch_all(mysqli_query($connection, $sql), MYSQLI_ASSOC)[0]);
+}
+
 ?>
